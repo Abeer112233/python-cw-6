8b4f9a4a48859dfcd1a9b218ee3e8e2bb49f7e24
Abeer112233 <khawatert1@gmail.com>
2023-09-06 19:44:07 +0300

    CW6

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# write your code here
+# part1
+person = {
+    "name": "aryam",
+    "age": 13,
+    "hobbies": ["swimming", "basketball", "horse riding"]
+}
+
+print("name:", person.get("name"), "age:", person.get("age"))
+# part2   
+person["age"]=14
+person["country"]="kuwait"
+
+print(person)
+print(len(person))
+# part3
+person["hobbies"].append("football")
+print(person)
+def check_hobbies(person):
+    if len(person["hobbies"])>=3:
+        return "WOW YOU ARE AMAZING"
+    else:
+        return "YOUR STILL AMAZING"
+
+print(check_hobbies(person))
